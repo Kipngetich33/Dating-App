ef218ad923f24ebf7810ba0b1e4028a25cef7c7b
Kipngetich33 <khalifngeno@gmail.com>
2018-01-08 07:53:14 -0500

    update the user model to hash and verify passwords

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
-from app import create_app, db
+from app import create_app, db  
 from flask_script import Manager,Server
-from app.models import User
+from app.models import User, Role
+from  flask_migrate import Migrate, MigrateCommand
 
 #creating the app instance
 app = create_app('development')
@@ -10,9 +11,13 @@ manager = Manager(app)
 manager.add_command('server',Server)
 
 
+#creating an instance of the app instance
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app, db = db, User = User ,Role = Role)
 
 if __name__ == '__main__':
     manager.run()
