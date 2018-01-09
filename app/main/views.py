549460b8e8b379bed616a946ca552332e47fc5dc
Kipngetich33 <khalifngeno@gmail.com>
2018-01-09 03:17:21 -0500

    add the create profile view function and template

--- app/main/views.py
+++ app/main/views.py
@@ -3,6 +3,7 @@ from . import main
 from ..models import User 
 from flask_login import login_required, current_user
 from .. import db
+from .forms import ReviewForm,UpdateProfile
 
 @main.route('/')
 def index():
@@ -12,11 +13,12 @@ def index():
     return render_template('index.html')
 
 
-@main.route('/profile/')
-@login_required
-def profile():
-    '''
-    Root function that return the logged in user profile template and its data
-    '''
-    pass
+@main.route('/Profile/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
 
